6f3d466d9f72433b828e0ebc77db92905afd6307
Ty Hill <crazysnoheadman@yahoo.com>
2015-12-14 23:15:07 -0700

    Day 1 steps complete

---
+++ homeCtrl.js
@@ -0,0 +1,18 @@
+angular.module('devMtIn')
+.controller('homeCtrl', function($scope) {
+
+  $scope.myProfile = {
+      friends: [{name: 'Ryan'}, {name: 'Bryan'}, {name: 'Sarah'}, {name: 'Zac'}, {name: 'Erin'}]
+  };
+
+  $scope.sortOptions = [{
+      display: 'Ascending',
+      value: false
+    },
+    {
+      display: 'Descending',
+      value: true
+    }
+  ];
+
+});
